c2c4199f6f72bbc4ca55ebf435bbbcf6be82eba7
verasmarianna <mariannamaldiveras@gmail.com>
2023-08-26 15:16:08 -0300

    primeira integracao

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -14,7 +14,7 @@ export function Content() {
   const [citacao, setCitacao] = useState('')
   const [imagem, setImagem] = useState('')
   const [success, setSuccess] = useState(false)
-  const baseURL = 'https://back-end-6c7c.onrender.com/mulheres'
+  const baseURL = 'https://back-end-7kk8.onrender.com/mulheres'
 
   useEffect(() => {
     async function getData() {
